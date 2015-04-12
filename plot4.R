3289365b732b45a78a9ba8940ad207aba9b5d146
TTBISIKLI <burak.isikli@gmail.com>
2015-04-12 23:40:03 +0300

    Bugfix

--- plot4.R
+++ plot4.R
@@ -11,17 +11,18 @@ data <- subset(data, subset=(Date >= "2007-02-01"
 data$Datetime <- as.POSIXct(paste(data$Date, data$Time))
 
 ## Plot 4
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(1,1,2,2))
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
 with(data, {
   plot(Global_active_power~Datetime, type="l", 
-       ylab="Global Active Power", xlab="")
+       ylab="Global Active Power", xlab="", cex=0.2)
   plot(Voltage~Datetime, type="l", 
        ylab="Voltage", xlab="datetime")
   plot(Sub_metering_1~Datetime, type="l", 
        ylab="Global Active Power", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+  legend("topright", col=c("black", "red", "blue"), lwd=2.5, bty="o",
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch=21, cex=0.5)
   plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global_Rective_Power",xlab="datetime")
