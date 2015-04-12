3dc26ced120f96aae68d550cf844f70f7cbe0726
TTBISIKLI <burak.isikli@gmail.com>
2015-04-12 21:51:20 +0300

    Change object type

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ data <- subset(data, subset=(as.Date(data$Date, format="%d/%m/%Y") >= "2007-02-0
                              & as.Date(data$Date, format="%d/%m/%Y") <= "2007-02-02"))
 
 ## Plot 1
-hist(as.numeric(data$Global_active_power), main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+hist(data$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ## Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
